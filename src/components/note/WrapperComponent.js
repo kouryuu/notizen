0bc6ffdad47974feea2dafaf89f3d9bd67b78194
Rodrigo Reyes <samurai@erdos.local>
2016-12-21 01:32:55 -0600

    Added a wrapper component

---
+++ src/components/note/WrapperComponent.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+import Body from 'BodyComponent';
+import Tag from 'TagComponent';
+require('styles/note/Wrapper.css');
+
+class WrapperComponent extends React.Component {
+  render() {
+    return (
+      <div className="wrapper-component">
+        <Tag></Tag><Body></Body>
+      </div>
+    );
+  }
+}
+
+WrapperComponent.displayName = 'NoteWrapperComponent';
+
+// Uncomment properties you need
+// WrapperComponent.propTypes = {};
+// WrapperComponent.defaultProps = {};
+
+export default WrapperComponent;
