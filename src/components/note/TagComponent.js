9048c7e66661de2c0ce08a0eda6b92136b2fe5c9
Rodrigo Reyes <samurai@erdos.local>
2016-12-21 01:32:17 -0600

    Added Tag Component

---
+++ src/components/note/TagComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/note/Tag.css');
+
+class TagComponent extends React.Component {
+  render() {
+    return (
+      <div className="tag-component">
+        Please edit src/components/note//TagComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+TagComponent.displayName = 'NoteTagComponent';
+
+// Uncomment properties you need
+// TagComponent.propTypes = {};
+// TagComponent.defaultProps = {};
+
+export default TagComponent;
