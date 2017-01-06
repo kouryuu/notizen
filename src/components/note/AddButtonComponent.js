a9450226d74ce5fe56538a59ba6486ed6efe233c
Rodrigo Reyes <samurai@erdos.local>
2017-01-06 14:38:03 -0600

    Added 'AddButton" component.

---
+++ src/components/note/AddButtonComponent.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/note/AddButton.css');
+
+class AddButtonComponent extends React.Component {
+  render() {
+    return (
+      <span className="addbutton-component">
+        <a href="#" onClick={ this.props.clickHandler } >
+        [ + ]
+        </a>
+      </span>
+    );
+  }
+}
+
+AddButtonComponent.displayName = 'NoteAddButtonComponent';
+
+// Uncomment properties you need
+// AddButtonComponent.propTypes = {};
+// AddButtonComponent.defaultProps = {};
+
+export default AddButtonComponent;
