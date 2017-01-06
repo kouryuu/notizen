8c4e69402993d3eccc936b76b3c65f4918d29ccb
Rodrigo Reyes <samurai@erdos.local>
2017-01-06 15:05:37 -0600

    Added note add behaviour.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,26 @@ require('styles/App.css');
 import React from 'react';
 import Note from './note/WrapperComponent'
 import NoteTitle from './note/TitleComponent'
+import AddButton from './note/AddButtonComponent'
 
 class AppComponent extends React.Component {
+  constructor(){
+  super();
+    this.state = {
+      notes: [<Note></Note>]
+    }
+  }
+  addNewNote(){
+    let notes_array = this.state.notes;
+    notes_array.push(<Note></Note>);
+    this.setState({notes: notes_array});
+  }
   render() {
     return (
       <div className="index">
         <NoteTitle title={ new Date().toDateString() }></NoteTitle>
-        <Note ></Note>
+        { this.state.notes }
+        <AddButton clickHandler={ this.addNewNote.bind(this) }></AddButton>
       </div>
     );
   }
