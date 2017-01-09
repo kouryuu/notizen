0fb4d2e666d12099becd3a959adeba48ca7cd4d0
Rodrigo Reyes <samurai@erdos.local>
2017-01-09 00:04:08 -0600

    Improved the BodyComponent
    The BodyComponent can now be resized.

--- src/components/note/BodyComponent.js
+++ src/components/note/BodyComponent.js
@@ -8,16 +8,25 @@ class BodyComponent extends React.Component {
   constructor() {
     super();
     this.state = {
-      text: ''
+      text: '',
+      lines: 1
     };
   }
   ChangeValue(event) {
     this.setState({text:event.target.value});
   }
+  GrowInput(event) {
+    if(event.key === 'Enter' || event.key === 'Backspace'){
+      this.setState({lines: this.getNumberOfLines(this.state.text)});
+    }
+  }
+  getNumberOfLines(text) {
+    return text.split('\n').length;
+  }
   render() {
     return (
       <div className="body-wrapper">
-      <input type="text" className="body-component" placeholder={ this.props.text } value={ this.state.text } onChange={ this.ChangeValue.bind(this) } />
+      <textarea type="text" className="body-component" placeholder={ this.props.text } value={ this.state.text } onChange={ this.ChangeValue.bind(this) } onKeyUp={ this.GrowInput.bind(this) } rows={ this.state.lines }/>
       </div>
     );
   }
