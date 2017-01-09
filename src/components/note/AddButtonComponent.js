ff9747c16bf0fd72b8eb4e379b826a94732865f4
Rodrigo Reyes <samurai@erdos.local>
2017-01-08 23:38:51 -0600

    Improved the look of the AddButton component.

--- src/components/note/AddButtonComponent.js
+++ src/components/note/AddButtonComponent.js
@@ -7,11 +7,11 @@ require('styles/note/AddButton.css');
 class AddButtonComponent extends React.Component {
   render() {
     return (
-      <span className="addbutton-component">
+      <div className="addbutton-component">
         <a href="#" onClick={ this.props.clickHandler } >
         <i className="fa fa-plus-circle" aria-hidden="true"></i>
         </a>
-      </span>
+      </div>
     );
   }
 }
