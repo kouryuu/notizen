48cbdfa8ad8c375c0fa1e2f6c7c7cbe859647499
Rodrigo Reyes <rodrigo@plethora.com.mx>
2017-01-13 14:04:11 -0600

    Added controls to TimerTag Component
    Added the button to increase the hours and minutes.
    Added the button to decrease the hours and minutes.
    Added toggle behavior to the controls components.

--- src/components/note/TimeTagComponent.js
+++ src/components/note/TimeTagComponent.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import React from 'react';
+import AddButton from './AddButtonComponent'
+import RemoveButton from './RemoveButtonComponent'
 
 require('styles/note/TimeTag.css');
 
@@ -9,17 +11,43 @@ class TimeTagComponent extends React.Component {
     super();
     this.state = {
       hour: new Date().getHours(),
-      minute: new Date().getMinutes()
+      minute: new Date().getMinutes(),
+      controlsDisplayed: {
+          display: 'none'
+        }
     };
   }
+
   zeroedNumber(number) {
     return (parseInt(number) > 9)?number:'0'+number;
   }
   render() {
+    let toggleControls= function(event) {
+      event.preventDefault();
+      console.log(this.state.controlsDisplayed.display);
+      if(this.state.controlsDisplayed.display === 'none'){
+        this.setState({controlsDisplayed: {display: 'flex'}});
+      }
+      if(this.state.controlsDisplayed.display === 'flex'){
+        this.setState({controlsDisplayed: {display: 'none'}});
+      }
+    };
     return (
       <div className="timetag-component-wrapper">
-        <div className="timertag-component" onClick={ this.toggleControls() }>
+        <div className="timertag-component" >
+          <a href="#" onClick={ toggleControls.bind(this) } >
           { this.zeroedNumber(this.state.hour) } {':'} { this.zeroedNumber(this.state.minute) }
+          </a>
+        </div>
+        <div className="controls" style={ this.state.controlsDisplayed }>
+          <div className="hour-control-buttons">
+            <AddButton clickHandler={ this.increaseHour.bind(this) }></AddButton>
+            <RemoveButton clickHandler={ this.decreaseHour.bind(this) }></RemoveButton>
+          </div>
+          <div className="minute-control-buttons">
+            <AddButton clickHandler={ this.increaseMinute.bind(this) }></AddButton>
+            <RemoveButton clickHandler={ this.decreaseMinute.bind(this) }></RemoveButton>
+          </div>
         </div>
       </div>
     );
