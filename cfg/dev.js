3054055abb78b9e3087c744f060a812b034dba38
Rodrigo Reyes <rodrigo@plethora.com.mx>
2017-01-16 22:27:47 -0600

    Removed unnecessary loader.

--- cfg/dev.js
+++ cfg/dev.js
@@ -40,10 +40,6 @@ config.module.loaders.push({
   loader: "url-loader?limit=10000&minetype=application/font-woff"
 });
 config.module.loaders.push({
-  test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-  loader: "url-loader?limit=10000&minetype=application/font-woff"
-});
-config.module.loaders.push({
   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
   loader: "file-loader"
 });
