d3e7e1ac8229a61d7edd54bbfc223dcd71816a23
Rodrigo Reyes <rodrigo@plethora.com.mx>
2017-01-16 18:56:18 -0600

    Added NoteSettings component to WrapperComponent

--- src/components/note/WrapperComponent.js
+++ src/components/note/WrapperComponent.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import Body from './BodyComponent';
 import Tag from './TagComponent';
+import Settings from './NoteSettingsComponent';
 require('styles/note/Wrapper.css');
 
 class WrapperComponent extends React.Component {
   render() {
     return (
       <div className="wrapper-component">
-        <Tag></Tag><Body text="Write something here."></Body>
+        <Tag></Tag><Body text="Write something here."></Body><Settings></Settings>
       </div>
     );
   }
