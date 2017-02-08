7ec3b1a03eb478a7abc7e88af669cf2e413ed651
Rodrigo Reyes <rodrigo@plethora.com.mx>
2017-02-08 03:00:35 -0600

    Added Actions map.
    Mapped actions to constants for cleaner code.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,18 +3,14 @@
 /*
  * action types
  */
-
-export const UPDATE_TEXT = 'UPDATE_TEXT'
-export const CHANGE_LABEL_TYPE = 'CHANGE_LABEL_TYPE'
-
-
-/*
- * action creators
- */
-
-export function updateText(text) {
-  return { type: UPDATE_TEXT, text }
-}
-export function changeLabelType(type) {
-  return { type: UPDATE_TEXT, type }
-}
+export const ADD_NOTE = 'ADD_NOTE';
+export const CHANGE_CURRENT_TAG_TYPE = 'CHANGE_CURRENT_TAG_TYPE';
+export const GET_NOTES = 'GET_NOTES';
+export const GET_REACT_NOTES = 'GET_REACT_NOTES';
+export const CHANGE_NOTE_TAG_TYPE = 'CHANGE_NOTE_TAG_TYPE';
+export const REPLACE_TAG = 'REPLACE_TAG';
+export const GET_NOTE_TAG = 'GET_NOTE_TAG';
+export const CHANGE_BODY = 'CHANGE_BODY';
+export const GET_NOTE_BODY = 'GET_NOTE_BODY';
+export const CHANGE_CURRENT_PAGE_TITLE = 'CHANGE_CURRENT_PAGE_TITLE';
+export const GET_CURRENT_PAGE_TITLE = 'GET_CURRENT_PAGE_TITLE';
